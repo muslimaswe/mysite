9944ec7c0adca6d50df74e8ab58ea529d033b6a0
muslimaswe <158568263+muslimaswe@users.noreply.github.com>
2024-02-02 15:40:36 +0500

    Initial commit

---
+++ blog/forms.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+
+
+class CreateUserFrom(UserChangeForm):
+    first_name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control form-icon-input',
+               'placeholder': 'Ism', 'type': 'text'}
+    ))
+
+    last_name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control form-icon-input',
+               'placeholder': 'Familiya', 'type': 'text'}
+    ))
+
+    password1 = forms.CharField(max_length=255, required=True, widget=forms.PasswordInput(
+       attrs={'class': 'form-control form-icon-input',
+              'placeholder': 'Parol', 'type': 'password'}
+    ))
+
+    password2 = forms.CharField(max_length=255, required=True, widget=forms.PasswordInput(
+       attrs={'class': 'form-control form-icon-input',
+              'placeholder': 'Parolni tasdiqlash', 'type': 'password'}
+    ))
+
+
+    class Meta(UserChangeForm):
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control form-icon-input',
+               'placeholder': 'username'}
+    ))
+
+    password = forms.CharField(max_length=255, required=True, widget=forms.PasswordInput(
+       attrs={'class': 'form-control form-icon-input',
+              'placeholder': 'Parol', 'type': 'password'}
+    ))
